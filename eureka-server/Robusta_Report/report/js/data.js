25473e908212e9c36b2e0e6987f16c734f0247f7
killuo <34058386+killuo@users.noreply.github.com>
2018-01-08 13:58:22 +0800

    eureka

---
+++ eureka-server/Robusta_Report/report/js/data.js
@@ -0,0 +1,51 @@
+
+var robustaData = {
+	
+	"projectData": {
+		"name": "eureka-server",
+		"date": "2017/11/29 上午11時51分50秒",
+		"loc": "15",
+		"trynumber": "0",
+		"catchnumber": "0",
+		"finallynumber": "0"
+	},
+	
+	"allBadSmellData": [
+	
+		{	"packageName": 	"java/com.didispace", 
+			"codeLink":	"#/eureka-server/src/main/java/com/didispace/Application.java#11#", 
+			"className": 	"Application.java",
+			"methodName": 	"main",
+			"line":		"11",
+			"smellType":	"Unprotected Main Program"
+		}
+	]
+,
+	
+	"allPackageData": [
+		
+		[
+			"0",
+			"java/com.didispace",
+			"15",
+			"0",
+			"0",
+			"1",
+			"0",
+			"0",
+			"0",
+			"1"	
+		]
+	]
+,
+	
+	"badSmellTypeDensity": [
+		{"type": "Empty Catch Block", "count": "0"},
+		{"type": "Dummy Handler", "count": "0"},
+		{"type": "Unprotected Main Program", "count": "1"},
+		{"type": "Nested Try Statement", "count": "0"},
+		{"type": "Careless Cleanup", "count": "0"},
+		{"type": "Exception Thrown From Finally Block", "count": "0"}
+	]
+
+}
---
+++ eureka-server/target/eureka-server.bat
@@ -0,0 +1,3 @@
+color 0A
+title eureka-server
+java -jar "C:\Users\chriswu\Desktop\SpringCloudBook-master\eureka-server\target\eureka-server-1.0.0.jar"
